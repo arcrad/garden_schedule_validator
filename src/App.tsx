566a6ee0675cde8e6b6361a8675a41b8fe14defb
arcrad <jonathanmaiorana@gmail.com>
2023-01-29 22:37:11 -0500

    started building out basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,69 @@ import logo from './logo.svg';
 import './App.css';
 
 import {
-	plantData
-} from './PlantData';
+	PlantingPeriod,
+	Plant,
+	PlantData,
+	PlantScheduleEntry,
+	PlantSchedule
+} from './CommonTypes';
+import plantData from './PlantData';
+import plantSchedule from './PlantSchedule';
 
 function App() {
 	console.dir(plantData);
+
+	let PlantScheduleTableRows = (plantSchedule:PlantSchedule) => {
+		//group plant schedule by groupId
+		let scheduleGroups:PlantScheduleEntry[][] = [];
+		plantSchedule.forEach( (plantScheduleEntry:PlantScheduleEntry) => {
+			if( scheduleGroups[plantScheduleEntry.groupId] == null ) {
+				scheduleGroups[plantScheduleEntry.groupId] = [];
+			}
+			scheduleGroups[plantScheduleEntry.groupId].push(plantScheduleEntry);
+		});
+
+		return (
+			scheduleGroups.map((scheduleGroup:PlantScheduleEntry[], i:number) => (
+				scheduleGroup.map( (plantScheduleEntry:PlantScheduleEntry, i:number, allSchedulesInGroup:PlantScheduleEntry[]) => (
+					<tr key={'pse'+i}>
+						{(() => { 
+							if(i == 0) return (<td rowSpan={allSchedulesInGroup.length}>{plantScheduleEntry.groupId}</td>)
+						})()}
+						<td>{plantScheduleEntry.plantId}</td>
+						<td>timeline for {plantScheduleEntry.plantId}</td>
+					</tr>
+				))
+			))
+		);
+	};
+
+	let PlantScheduleTable = (plantSchedule:PlantSchedule) => {
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>Group</th>
+						<th>Plant Name</th>
+						<th>Timeline</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						PlantScheduleTableRows(plantSchedule)
+					}
+				</tbody>
+			</table>
+		)
+	};
+	
   return (
     <div className="App">
 			<h1>Garden Schedule Validator</h1>
 			<p>Create and validate garden schedules</p>
+			{
+				PlantScheduleTable(plantSchedule)
+			}
     </div>
   );
 }
