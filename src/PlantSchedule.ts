e90bf8b2f0686bb5eb4de98dd8e75f258fe04c55
arcrad <jonathanmaiorana@gmail.com>
2023-01-29 22:36:52 -0500

    created inital plant schedule data for testing

---
+++ src/PlantSchedule.ts
@@ -0,0 +1,34 @@
+import {
+	PlantScheduleEntry,
+	PlantSchedule
+} from './CommonTypes';
+
+const plantSchedule:PlantSchedule = [
+	{
+		plantId: 0,
+		groupId: 0,
+		numberOfPlants: 24
+	},
+	{
+		plantId: 1,
+		groupId: 0,
+		numberOfPlants: 30
+	},
+	{
+		plantId: 0,
+		groupId: 1,
+		numberOfPlants: 30
+	},
+	{
+		plantId: 2,
+		groupId: 2,
+		numberOfPlants: 10
+	},
+	{
+		plantId: 1,
+		groupId: 2,
+		numberOfPlants: 12
+	},
+];
+
+export default plantSchedule;
