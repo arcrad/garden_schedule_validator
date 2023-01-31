5c35c66d73b4384129943046af62cd4afcd9f337
arcrad <jonathanmaiorana@gmail.com>
2023-01-30 23:50:36 -0500

    updated schedule with more actual data

--- src/PlantSchedule.ts
+++ src/PlantSchedule.ts
@@ -7,27 +7,63 @@ const plantSchedule:PlantSchedule = [
 	{
 		plantId: 'Peas, Garden',
 		groupId: 0,
-		numberOfPlants: 24
+		numberOfPlants: 15
 	},
 	{
 		plantId: 'Spinach',
 		groupId: 0,
-		numberOfPlants: 30
+		numberOfPlants: 24
 	},
 	{
 		plantId: 'Beets',
-		groupId: 1,
+		groupId: 0,
+		numberOfPlants: 6
+	},
+	{
+		plantId: 'Cucumbers',
+		groupId: 0,
+		numberOfPlants: 5
+	},
+	{
+		plantId: 'Cucamelon',
+		groupId: 0,
+		numberOfPlants: 4
+	},
+	{
+		plantId: 'Malabar Spinach',
+		groupId: 0,
+		numberOfPlants: 6
+	},
+	{
+		plantId: 'Chinese Broccoli',
+		groupId: 0,
+		numberOfPlants: 7
+	},
+	{
+		plantId: 'Lettuce, Head',
+		groupId: 0,
 		numberOfPlants: 30
 	},
+	//
 	{
-		plantId: 'Spinach',
-		groupId: 2,
-		numberOfPlants: 10
+		plantId: 'Peas, Garden',
+		groupId: 1,
+		numberOfPlants: 15
 	},
 	{
-		plantId: 'Broccoli',
-		groupId: 2,
-		numberOfPlants: 12
+		plantId: 'Chinese Broccoli',
+		groupId: 1,
+		numberOfPlants: 7
+	},
+	{
+		plantId: 'Lettuce, Head',
+		groupId: 1,
+		numberOfPlants: 4
+	},
+	{
+		plantId: 'Lettuce, Leaf', //will be leaf
+		groupId: 1,
+		numberOfPlants: 2
 	},
 ];
 
