6e9eba2f0ae88aab14b05c8e7bd3ab052250fd8d
arcrad <jonathanmaiorana@gmail.com>
2023-01-30 22:01:44 -0500

    added more actual plant data

--- src/PlantData.ts
+++ src/PlantData.ts
@@ -20,6 +20,12 @@ const plantData:PlantData = [
 				plantEndDate: new Date(2000, 3, 15),
 				harvestStartDate: new Date(2000, 3, 5),
 				harvestEndDate: new Date(2000, 5, 5)
+			},
+			{
+				plantStartDate: new Date(2000, 7, 25),
+				plantEndDate: new Date(2000, 9, 15),
+				harvestStartDate: new Date(2000, 8, 25),
+				harvestEndDate: new Date(2000, 11, 31)
 			}
 		]
 	},
@@ -31,6 +37,12 @@ const plantData:PlantData = [
 				plantEndDate: new Date(2000, 4, 5),
 				harvestStartDate: new Date(2000, 4, 15),
 				harvestEndDate: new Date(2000, 6, 5)
+			},
+			{
+				plantStartDate: new Date(2000, 7, 5),
+				plantEndDate: new Date(2000, 8, 15),
+				harvestStartDate: new Date(2000, 8, 25),
+				harvestEndDate: new Date(2000, 10, 25)
 			}
 		]
 	}
